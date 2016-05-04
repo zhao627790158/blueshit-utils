307f20c4018e5118d732f2ae547bbef7011f4b2e
zhaoheng9 <zhaoheng@jd.com>
2016-05-04 10:31:42 +0800

    first

---
+++ common-utils/src/main/java/cn/zh/blueshit/commonutils/BeanUtils.java
@@ -0,0 +1,37 @@
+package cn.zh.blueshit.commonutils;
+
+
+import org.apache.commons.beanutils.PropertyUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Map;
+
+/**
+ * Created by zhaoheng on 2016/4/25.
+ */
+public class BeanUtils {
+    private static final Logger logger = LoggerFactory.getLogger(BeanUtils.class);
+
+    public BeanUtils() {
+    }
+
+
+    public static Map convertObject2Map(Object obj) {
+        try {
+            return org.apache.commons.beanutils.BeanUtils.describe(obj);
+        } catch (Exception var2) {
+            logger.error("将对象转换为map时发生异常",var2);
+            throw new RuntimeException("将对象转换为map时发生异常", var2);
+        }
+    }
+    public static void copyProperties(Object dest, Object orig) {
+        try {
+            PropertyUtils.copyProperties(dest, orig);
+        } catch (Exception var3) {
+            logger.error(var3.getMessage());
+            throw new RuntimeException("对象属性复制时出现异常!", var3);
+        }
+    }
+
+}
---
+++ common-utils/src/main/java/cn/zh/blueshit/commonutils/GsonUtils.java
@@ -0,0 +1,156 @@
+package cn.zh.blueshit.commonutils;
+
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.*;
+
+/**
+ * Created by zhaoheng on 2016/4/25.
+ */
+public class GsonUtils {
+
+    private static final String JSON_EMPTY = "{}";
+    private static final String JSON_EMPTY_ARRAY = "[]";
+    private static final String DEFAULT_DATE_PATTERN = "yyyy-MM-dd HH:mm:ss";
+
+    public static String toJson(Object target) {
+        return toJson(target, (Type) null, (String) null);
+    }
+
+    public static String toJson(Object target, Type targetType) {
+        return toJson(target, targetType, (String) null);
+    }
+
+    public static String toJson(Object target, Type targetType, String datePattern) {
+        if (null == target) {
+            return JSON_EMPTY;
+        }
+        GsonBuilder builder = new GsonBuilder();
+        if (datePattern == null || datePattern.length() < 1) {
+            datePattern = DEFAULT_DATE_PATTERN;
+        }
+
+        builder.setDateFormat(datePattern);
+        Gson gson = builder.create();
+        String result = "{}";
+
+        try {
+            if (targetType == null) {
+                result = gson.toJson(target);
+            } else {
+                result = gson.toJson(target, targetType);
+            }
+        } catch (Exception var7) {
+            if (target instanceof Collection || target instanceof Iterator || target instanceof Enumeration || target.getClass().isArray()) {
+                result = JSON_EMPTY_ARRAY;
+            }
+        }
+        return result;
+
+    }
+
+    public static <T> T fromJson(String json, TypeToken<T> token, String datePattern) {
+        if (json != null && json.length() >= 1) {
+            GsonBuilder builder = new GsonBuilder();
+            if (datePattern == null || datePattern.length() < 1) {
+                datePattern = DEFAULT_DATE_PATTERN;
+            }
+
+            builder.setDateFormat(datePattern);
+            Gson gson = builder.create();
+
+            try {
+                return gson.fromJson(json, token.getType());
+            } catch (Exception var6) {
+                return null;
+            }
+        } else {
+            return null;
+        }
+    }
+
+
+    public static Object fromJson(String json, Type type, String datePattern) {
+        if (json != null && json.length() >= 1) {
+            GsonBuilder builder = new GsonBuilder();
+            if (datePattern == null || datePattern.length() < 1) {
+                datePattern = DEFAULT_DATE_PATTERN;
+            }
+
+            builder.setDateFormat(datePattern);
+            Gson gson = builder.create();
+
+            try {
+                return gson.fromJson(json, type);
+            } catch (Exception var6) {
+                return null;
+            }
+        } else {
+            return null;
+        }
+    }
+
+    public static Object fromJson(String json, Type type) {
+        return fromJson(json, (Type) type, (String) null);
+    }
+
+    public static <T> T fromJson(String json, TypeToken<T> token) {
+        return (T)fromJson(json, (TypeToken) token, (String) null);
+    }
+
+    public static <T> T fromJson(String json, Class<T> clazz, String datePattern) {
+        if (json != null && json.length() >= 1) {
+            GsonBuilder builder = new GsonBuilder();
+            if (datePattern == null || datePattern.length() < 1) {
+                datePattern = DEFAULT_DATE_PATTERN;
+            }
+
+            builder.setDateFormat(datePattern);
+            Gson gson = builder.create();
+
+            try {
+                return gson.fromJson(json, clazz);
+            } catch (Exception var6) {
+                return null;
+            }
+        } else {
+            return null;
+        }
+    }
+
+    public static <T> T fromJson(String json, Class<T> clazz) {
+        return (T)fromJson(json, (Class) clazz, (String) null);
+    }
+
+    public static void main(String[] args) {
+        //基本的数据类型无需TypeToken
+        Integer i = new Integer(1);
+        String i_1 = toJson(i);
+        Integer integer = fromJson(i_1, Integer.class);
+        System.out.println(integer);
+        //list<map>-->json
+        HashMap map = new HashMap();
+        map.put("id", Integer.valueOf(1));
+        HashMap map2 = new HashMap();
+        map2.put("id", Integer.valueOf(2));
+        ArrayList list = new ArrayList();
+        list.add(map);
+        list.add(map2);
+        String json2 = toJson(list,new TypeToken<List<HashMap>>(){}.getType());
+        System.out.println(json2);
+        //json--->list<map>
+        Object o = fromJson(json2, new TypeToken<List<HashMap>>() {
+        }.getType());
+
+        HashMap zhmap = new HashMap();
+        zhmap.put("test", "test111");
+        String zh = toJson(zhmap, (new TypeToken<HashMap>(){}).getType(), "yyyy-MM-dd HH:mm:ss");
+        System.out.println(zh);
+        System.out.println(toJson(zhmap));
+
+    }
+}
