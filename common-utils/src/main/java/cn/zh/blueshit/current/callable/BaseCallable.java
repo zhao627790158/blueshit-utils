3c0439b06cfb4431bd0ba35005db2e7ab33a1651
zhaoheng9 <zhaoheng@jd.com>
2016-06-12 17:42:16 +0800

    修改baseCallable

--- common-utils/src/main/java/cn/zh/blueshit/current/callable/BaseCallable.java
+++ common-utils/src/main/java/cn/zh/blueshit/current/callable/BaseCallable.java
@@ -1,13 +1,48 @@
 package cn.zh.blueshit.current.callable;
 
 import java.util.concurrent.Callable;
+import java.util.concurrent.CountDownLatch;
 
 /**
  * Created by zhaoheng on 2016/6/3.
  */
 public abstract class BaseCallable<T> implements Callable<T> {
 
-    @Override
-    public abstract T call() throws Exception;
+    private CountDownLatch latch;
+
+    /**
+     * 线程超时时间，默认为3000ms
+     */
+    private long timeout = 3000;
+
+    public T call() throws Exception {
+        T o = null;
+        try {
+            o = execute();
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            latch.countDown();
+        }
+        return o;
+    }
+
+    public abstract T execute() throws Exception;
+
+    public CountDownLatch getLatch() {
+        return latch;
+    }
+
+    public void setLatch(CountDownLatch latch) {
+        this.latch = latch;
+    }
+
+    public long getTimeout() {
+        return timeout;
+    }
+
+    public void setTimeout(long timeout) {
+        this.timeout = timeout;
+    }
 
 }
