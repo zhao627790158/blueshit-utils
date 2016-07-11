091f0dbe5c99e94c37b415e0af9d35a2804cab7f
zhaoheng9 <zhaoheng@jd.com>
2016-07-11 18:20:54 +0800

    random

--- common-utils/src/main/java/cn/zh/blueshit/security/tool/RandomKeys.java
+++ common-utils/src/main/java/cn/zh/blueshit/security/tool/RandomKeys.java
@@ -1,5 +1,7 @@
 package cn.zh.blueshit.security.tool;
 
+import org.apache.commons.lang.RandomStringUtils;
+
 import java.util.Random;
 
 /**
@@ -25,11 +27,35 @@ public class RandomKeys {
         return sbBuffer.toString();
     }
 
+    /**
+     * //产生5位长度的随机字符串，中文环境下是乱码
+     RandomStringUtils.random(5);
+     //使用指定的字符生成5位长度的随机字符串
+     RandomStringUtils.random(5, new char[]{'a','b','c','d','e','f', '1', '2', '3'});
+     //生成指定长度的字母和数字的随机组合字符串
+     RandomStringUtils.randomAlphanumeric(5);
+     //生成随机数字字符串
+     RandomStringUtils.randomNumeric(5);
+     //生成随机[a-z]字符串，包含大小写
+     RandomStringUtils.randomAlphabetic(5);
+     //生成从ASCII 32到126组成的随机字符串
+     RandomStringUtils.randomAscii(4)
+     * @param count
+     * @return
+     */
+
+    public static String getRandomStringByLength(int count) {
+        return RandomStringUtils.randomAlphanumeric(count);
+    }
+
     public static void main(String[] args) {
         for (int i = 0; i < 100; i++) {
             initKey();
+            System.out.println(getRandomStringByLength(32).toCharArray());
         }
 
     }
+
+
 
 }
