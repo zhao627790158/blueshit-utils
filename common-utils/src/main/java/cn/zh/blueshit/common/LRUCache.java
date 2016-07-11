daa2fb7f227c518424463ae451ed351c8c6f15e5
zhaoheng9 <zhaoheng@jd.com>
2016-07-11 15:11:36 +0800

    修改Gsonconvert

---
+++ common-utils/src/main/java/cn/zh/blueshit/common/LRUCache.java
@@ -0,0 +1,144 @@
+package cn.zh.blueshit.common;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * Created by zhaoheng on 2016/7/11.
+ * 利用LinkedHashMap实现简单的缓存， 必须实现removeEldestEntry方法，具体参见JDK文档
+ * 按插入顺序的链表，和按访问顺序(调用get方法)的链表。
+ * 默认是按插入顺序排序，如果指定按访问顺序排序，那么调用get方法后，会将这次访问的元素移至链表尾部，不断访问可以形成按访问顺序排序的链表
+ * 可以重写removeEldestEntry方法返回true值指定插入元素时移除最老的元素
+ */
+public class LRUCache<K, V> extends LinkedHashMap<K, V> {
+
+    private final int maxCapacity;
+    private static final float DEFAULT_LOAD_FACTOR = 0.75f;
+    private final Lock lock = new ReentrantLock();
+
+
+    public LRUCache(int maxCapacity) {
+        /**
+         * * @param  accessOrder     the ordering mode - <tt>true</tt> for access-order, <tt>false</tt> for insertion-order
+         * true表示按照访问顺序排序
+         */
+        super(maxCapacity, DEFAULT_LOAD_FACTOR, true);
+        this.maxCapacity = maxCapacity;
+    }
+
+    //his method is invoked by put and putAll after inserting a new entry into the map.
+    @Override
+    protected boolean removeEldestEntry(java.util.Map.Entry<K, V> eldest) {
+        try {
+            lock.lock();
+            return size() > maxCapacity;
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    @Override
+    public boolean containsKey(Object key) {
+        try {
+            lock.lock();
+            return super.containsKey(key);
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    @Override
+    public boolean containsValue(Object value) {
+        try {
+            lock.lock();
+            return super.containsValue(value);
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    @Override
+    public V get(Object key) {
+        try {
+            lock.lock();
+            return super.get(key);
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    @Override
+    public V put(K key, V value) {
+        try {
+            lock.lock();
+            return super.put(key, value);
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    @Override
+    public void putAll(Map<? extends K, ? extends V> m) {
+        try {
+            lock.lock();
+            super.putAll(m);
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    @Override
+    public int size() {
+        try {
+            lock.lock();
+            return super.size();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    @Override
+    public boolean isEmpty() {
+        try {
+            lock.lock();
+            return super.isEmpty();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    @Override
+    public V remove(Object key) {
+        try {
+            lock.lock();
+            return super.remove(key);
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    @Override
+    public void clear() {
+        try {
+            lock.lock();
+            super.clear();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public Collection<Map.Entry<K, V>> getAll() {
+        try {
+            lock.lock();
+            return new ArrayList<Map.Entry<K, V>>(super.entrySet());
+        } finally {
+            lock.unlock();
+        }
+    }
+
+
+}
---
+++ common-utils/src/test/java/cn/blueshit/cn/test/TestLRU.java
@@ -0,0 +1,29 @@
+package cn.blueshit.cn.test;
+
+import cn.zh.blueshit.common.LRUCache;
+
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Created by zhaoheng on 2016/7/11.
+ */
+public class TestLRU {
+
+    @org.junit.Test
+    public void testLru() {
+        LRUCache<Integer,String> cache = new LRUCache<Integer,String>(10);
+        for(int i =0;i<10;i++) {
+            cache.put(i, "test" + i);
+        }
+        cache.get(0);
+        cache.put(11,"test11");
+        Set<Map.Entry<Integer, String>> entries = cache.entrySet();
+        for (Map.Entry<Integer, String> map : entries) {
+            System.out.println("key: "+map.getKey()+"---value: "+map.getValue());
+        }
+
+
+
+    }
+}
