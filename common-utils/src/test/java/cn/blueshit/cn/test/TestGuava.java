c1d1df32a7d8726ede48ef4973045cd6e2adf181
zhaoheng <zhaoheng@zhaohengdeMacBook-Pro.local>
2016-08-18 16:04:32 +0800

    add test guava

--- common-utils/src/test/java/cn/blueshit/cn/test/TestGuava.java
+++ common-utils/src/test/java/cn/blueshit/cn/test/TestGuava.java
@@ -3,6 +3,7 @@ package cn.blueshit.cn.test;
 import com.google.common.base.Joiner;
 import com.google.common.collect.Lists;
 import com.google.common.util.concurrent.RateLimiter;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.time.DateUtils;
 
 import java.text.ParseException;
@@ -13,6 +14,7 @@ import java.util.List;
 /**
  * Created by zhaoheng on 16/8/18.
  */
+@Slf4j
 public class TestGuava {
 
     @org.junit.Test
@@ -66,5 +68,79 @@ public class TestGuava {
             limiter.acquire(); // 请求RateLimiter, 超过permits会被阻塞
             System.out.println(i);
         }
+    }
+
+    /*@org.junit.Test
+    public void test4() {
+        List<Integer> list = Lists.newArrayList(1, 2, 3, 4, 5, 6, 7);
+        final List<Integer> integers = Lists.newArrayList(1, 2, 3, 8);
+
+        //在list2也包含的
+        CollectionUtils.filter(list, new Predicate<Integer>() {
+            @Override
+            public boolean evaluate(Integer object) {
+                return integers.contains(object);
+            }
+        });
+        log.info("结果-integers-:{}", JSON.toJSONString(integers));
+        log.info("结果-list-:{}", JSON.toJSONString(list));
+        List<Integer> transform = Lists.transform(integers, new Function<Integer, Integer>() {
+            @Nullable
+            @Override
+            public Integer apply(Integer input) {
+                return input * 10;
+            }
+        });
+        log.info("结果-transform-:{}", JSON.toJSONString(transform));
+        log.info("是否相等:{}", test.ONE == test.ONE);
+
+
+        Person p1 = new Person("001", "zhang_san");
+        Person p2 = new Person("002", "li_si");
+
+        List<Person> personList = Lists.newArrayList();
+        personList.add(p1);
+        personList.add(p2);
+        // 将主键当作Map的Key
+        Map<String, Person> personMap = Maps.uniqueIndex(personList, new Function<Person, String>() {
+            @Nullable
+            @Override
+            public String apply(@Nullable Person input) {
+                return input.getId();
+            }
+        });
+        System.out.println("将主键当作Map的Key:" + JSON.toJSONString(personMap));
+    }*/
+
+    public static enum test {
+        ONE, TWO;
+    }
+
+    class Person {
+        private String Id;
+        private String name;
+
+        public Person(String Id, String name) {
+            this.Id = Id;
+            this.name = name;
+        }
+
+        public String getId() {
+            return Id;
+        }
+
+        public void setId(String id) {
+            Id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+
     }
 }
